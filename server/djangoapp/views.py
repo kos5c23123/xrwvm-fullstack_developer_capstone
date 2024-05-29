ee62601ce963d3ef8e9a425cc9bb0a9dad623105
altas <tantszhang1@gmail.com>
2024-05-28 22:01:56 -0400

    update lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -66,7 +66,7 @@ def get_cars(request):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = [
-        {"CarModel": car_model.name, "CarMake": car_model.car_make.name} 
+        {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
         for car_model in car_models
     ]
     return JsonResponse({"CarModels": cars})
